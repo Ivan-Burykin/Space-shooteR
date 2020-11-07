d2821e893aa012bb5fd1215857d53460b6ec6d37
Ivan <56451550+Ivan-Burykin@users.noreply.github.com>
2020-11-07 19:26:53 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,12 @@
 from pygame import *
 
-
 class Settings:
     def __init__(self):
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (85, 170, 255)
+
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
