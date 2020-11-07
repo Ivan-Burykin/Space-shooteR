eeb5f899ae91eb957ca61d2c04976693579295c9
Ivan <56451550+Ivan-Burykin@users.noreply.github.com>
2020-11-07 19:27:31 +0300

    Update ship.py

--- ship.py
+++ ship.py
@@ -4,10 +4,21 @@ class Ship():
     def __init__(self, screen):
         self.screen = screen
 
-        self.image = image.load('spaceship.bmp')
+        self.image = image.load('space-ship.png')
+        self.image = transform.scale(self.image, (150, 150))
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
+        self.screen_rect.centery = 350
         self.rect.bottom = self.screen_rect.bottom
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_right == True and self.rect.right <= 1200:
+            self.rect.centerx += 2
+        if self.moving_left == True and self.rect.left > 0:
+            self.rect.centerx -= 2
+
     def blitme(self):
         self.screen.blit(self.image, self.rect)
