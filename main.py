8120527bc83ff3178830a290a66c624091d1476d
Ivan <56451550+Ivan-Burykin@users.noreply.github.com>
2020-11-07 19:26:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,22 +4,21 @@ from ship import Ship
 import game_functions as gf
 
 
+
 def run_game():
     init()
     game_settings = Settings()
     s = display.set_mode((game_settings.screen_width, game_settings.screen_height))
     ship = Ship(s)
     display.set_caption('Battleship')
-    bg = game_settings.bg_color
 
     while True:
-        gf.check_events()
-        s.fill(bg)
-        ship.blitme()
-
-        display.flip()
+        gf.check_events(ship)
+        gf.update_screen(game_settings, s, ship)
+        ship.update()
 
 if __name__ == '__main__':
     run_game()
+
 
 
