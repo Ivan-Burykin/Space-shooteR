113f173bdf40801e337e458e6b29ae14cac06aa9
Ivan <56451550+Ivan-Burykin@users.noreply.github.com>
2020-11-21 19:31:42 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,12 +1,15 @@
-from pygame import *
-
 class Settings:
+    """Класс для хранения настроек игры"""
     def __init__(self):
+        """Инициализация настроек"""
+        # параметры экрана
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (85, 170, 255)
+        self.bg_color = (230, 230, 230)
 
-        self.bullet_speed_factor = 1
-        self.bullet_width = 3
-        self.bullet_height = 15
-        self.bullet_color = 60, 60, 60
+        # параметры пули
+        self.bullet_speed_factor = 2
+        self.bullet_width = 10
+        self.bullet_height = 90
+        self.bullet_color = 200, 60,  60
+        self.bullets_max = 3
